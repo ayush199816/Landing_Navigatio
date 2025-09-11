f17b727415a60020091fe6e23f55dbba3ca44783
Ayush Gupta <ayush199816@gmail.com>
2025-09-11 15:48:48 +0530

    fixing build

--- build.sh
+++ build.sh
@@ -4,6 +4,9 @@ set -e
 # Install dependencies
 npm install --legacy-peer-deps
 
+# Grant execute permissions to react-scripts
+chmod +x ./node_modules/.bin/react-scripts
+
 # Set CI to false and run build
 export CI=false
 npm run build
